32d8e0b2e33a9d508ba1cee50c2e0c22e13d3c84
AC-Stray <absocube@qq.com>
2021-07-28 15:19:42 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# codes
+ Liuxizai's OI Codes
---
+++ template.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+#define File(name) freopen(#name".in", "r", stdin); freopen(#name".out", "w", stdout);
+#define ri register int
+#define Int inline int
+#define Void inline void
+#define Bool inline bool
+#define LL inline long long
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+const int MOD = 998244353;
+const int inf = 0x3f3f3f3f;
+const ll infll = 0x3f3f3f3f3f3f3f3f;
+const double eps = 1e-10;
+const int N = 100005;
+template<typename T>
+inline T read(){
+	T n = 0; int f = 1; char ch = getchar();
+	while(!isdigit(ch)){
+		if(ch == '-') f = -1;
+		ch = getchar();
+	}
+	while(isdigit(ch)){
+		n = n * 10 + ch - '0';
+		ch = getchar();
+	}
+	return f * n;
+}
+template<typename T>
+void write(T n){
+	if(n/10) write(n/10);
+	putchar(n%10+'0');
+}
+void input() {}
+template<typename Type, typename... Types>
+void input(Type &arg, Types&... args){
+	arg = read<Type>();
+	input(args...);
+}
+namespace Main{
+	void Main(){
+		return;
+	}
+} // namespace
+int main(){
+#ifdef Liuxizai
+	freopen("in", "r", stdin);
+	freopen("out", "w", stdout);
+#endif // Liuxizai
+	Main::Main();
+	return 0;
+}
+
